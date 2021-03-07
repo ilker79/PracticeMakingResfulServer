cd0dd8bfac9a65ff5391713c00f4792aeec70efb
ilker <ilkerpeker79@yahoo.com>
2021-03-07 13:59:10 +0000

    deleted the weatherforecast

---
+++ Note.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace PracticeMakingResfulServer
+{
+    public class Note
+    {
+        // public DateTime Date { get; set; }
+
+        // private DateTime date;
+        // public DateTime Date
+        // {
+        //     get { return date; }
+        //     set { date = value; }
+        // }
+
+        private string text;
+
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+
+}
